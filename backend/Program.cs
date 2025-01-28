402bbe918e653c0a7e291f4ff283307fd4869405
robsonrosaeu <76790407+robsonrosaeu@users.noreply.github.com>
2025-01-27 23:28:50 -0300

    Add files via upload

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,18 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Adicionar serviços do Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Ativar Swagger no ambiente de desenvolvimento
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+    });
 }
+else
+{
+    // Ativar Swagger na produção
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty;  // Para acessar o Swagger diretamente na raiz
+    });
+}
+
+// Configuração da porta para ouvir no 8080
+app.Urls.Add("http://0.0.0.0:8080");
 
 app.UseHttpsRedirection();
 
